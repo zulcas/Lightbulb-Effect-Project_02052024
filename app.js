989c6838ce7e1ec2a6400c4dc3b4044e7b426c89
zulcas <zulemacasadogarcia@gmail.com>
2024-05-02 21:12:45 +0200

    ejercicio terminado

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
+
+//1)Seleccionamos bombilla
+const bulbClick = document.querySelector("#bulb");
+//2)Creamos evento
+bulbClick.addEventListener('click', getBulbOnOff);
+
+
+//3)Funcion getBulbOnOff
+function getBulbOnOff (){
+    console.log(document.querySelector(".bulb-off"));
+    if (!!document.querySelector(".bulb-off")){ //me es util el !! para convertir eso en boleano
+        bulbClick.classList.remove("bulb-off");
+        bulbClick.classList.add("bulb-on");
+    }
+    else{
+        bulbClick.classList.remove("bulb-on");
+        bulbClick.classList.add("bulb-off");
+    }
+
+}
